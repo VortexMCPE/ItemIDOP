3659e9ab66ed1e4810df83e9e4a87c1fc7b7c9bc
VortexMCPE <41498052+VortexMCPE@users.noreply.github.com>
2018-07-29 23:41:44 -0700

    Create ItemID.php

---
+++ src/ItemID/ItemID.php
@@ -0,0 +1,28 @@
+<?php
+# plugin hecho por KaitoDoDo
+namespace KaitoDoDo\ItemID;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerItemHeldEvent;
+
+class ItemID extends PluginBase implements Listener {
+
+        public $slogan = "Â§fÂ§l[Â§eITEM IDÂ§f]";
+        
+	public function onEnable()
+	{
+		  $this->getLogger()->info("Â§eItemsID by KaitoDoDo");
+                  $this->getServer()->getPluginManager()->registerEvents($this ,$this);
+        }
+        
+        public function ItemHeld(PlayerItemHeldEvent $event)
+        {
+            $pl = $event->getPlayer();
+            if($pl->isOp())
+            {
+            $pl->sendTip($this->slogan . " " . $event->getItem()->getId() . "Â§b:Â§f" . $event->getItem()->getDamage());
+            }
+        }
+
+}
